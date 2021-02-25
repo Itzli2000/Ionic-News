4c66ec28ab6e7b59adff80d747260a3ae1f9961c
Ariel  Molina <itzli2000@msn.com>
2021-02-25 13:52:40 -0600

    Feat: show news lists in tab 1

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from 'src/app/models/interfaces';
+import { NewsService } from '../../services/news.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  news: Article[] = [];
 
-  constructor() {}
+  constructor(private newsService: NewsService) {}
 
+  ngOnInit(): void {
+    this.newsService.getTopHeadlines().subscribe((resp) => {
+      this.news.push(...resp.articles);
+    });
+  }
 }
